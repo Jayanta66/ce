b224a588cd71055ea55274f7888ae001401e1d21
Jayanta66 <jayhalder18@gmail.com>
2025-06-21 10:54:49 +0530

    add

--- src/api/api.js
+++ src/api/api.js
@@ -2,12 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: `${import.meta.env.VITE_BACK_END_URL}/api`,
+    origin: 'https://jayanta66.github.io/**'
     withCredentials: true,
 });
-
- const corsOptions = {
-          origin: 'https://jayanta66.github.io',
-          credentials: true,
-        };
 
 export default api;
