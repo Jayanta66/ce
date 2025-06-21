2e77dda871418e540810e51997d91f69c11b2f29
Jayanta66 <jayhalder18@gmail.com>
2025-06-21 11:22:40 +0530

    add

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: `${import.meta.env.VITE_BACK_END_URL}/api`,
-    origin: 'https://jayanta66.github.io/**'
     withCredentials: true,
 });
 
